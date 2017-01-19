3b7d2aa40c77d42de7167cd10de82a0916053b53
scm-ns <scm197@rutgers.edu>
2017-01-19 16:11:52 -0500

    Add the FLAnimatedImage Pod. Add Pod to source control for easy of use
    	The pod will be used to crate the Gif Image View in the collection view cell

---
+++ Podfile
@@ -0,0 +1,7 @@
+platform :ios, '9.0'
+
+target 'gif_image_view' do
+	pod 'FLAnimatedImage', '~>1.0'
+end
+
+
---
+++ Pods/Target Support Files/Pods-gif_image_view/Pods-gif_image_view-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_gif_image_view : NSObject
+@end
+@implementation PodsDummy_Pods_gif_image_view
+@end
